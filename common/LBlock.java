67c6ff7fc6d45e45cb736f2ac5fb6b2ba3e60bbb
trebegin <trent.begin@colorado.edu>
2013-04-18 12:52:13 -0600

    Update LBlock.java
    Test change - Higher activate

--- common/LBlock.java
+++ common/LBlock.java
@@ -49,7 +49,7 @@ public class LBlock extends Block {
      */
     public boolean onBlockActivated(World world, int par2, int par3, int par4, EntityPlayer player, int par6, float par7, float par8, float par9) {
     	Vec3 vector = player.getPosition(1.0F);
-		player.setPositionAndUpdate(vector.xCoord, vector.yCoord + 50, vector.zCoord);
+		player.setPositionAndUpdate(vector.xCoord, vector.yCoord + 60, vector.zCoord);
 		
 		this.removeBlockByPlayer(world, player, par2, par3, par4);
     	return false;
